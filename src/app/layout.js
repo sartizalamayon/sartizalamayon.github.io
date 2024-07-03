4b32dc77368ce885f5eea01bd50ea8234f981782
unknown <sartizayo.on@gmail.com>
2024-07-04 02:28:35 +0600

    fonts

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,13 +3,12 @@ import "./globals.css";
 
 export const metadata = {
   title: "Home",
-  description: "Protfoilo of a software engineer",
+  description: "Protfoilo of a sSartiz Alam Ayon",
 };
-
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
+      <body className="bg-primary min-h-screen">
         {children}
       </body>
     </html>
--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,7 @@
 export default function Home() {
   return (
-    <main>
+    <main className="text-4xl">
       hey this is home
     </main>
-    
   );
 }
