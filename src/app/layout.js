3c62b0a55857056f26d4288dd402e6e927a16e57
ay0-0n <sartizayo.on@gmail.com>
2024-08-15 01:47:16 +0600

    bg4

--- src/app/layout.js
+++ src/app/layout.js
@@ -13,7 +13,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <link rel="icon" href="favicon.png" sizes="any" />
-      <body className="bg-primary min-h-screen dot-bg">
+      <body className="bg-primary min-h-screen chess-bg">
         {/* implement background */}
         <MyProvider>
         <ModalProvider>
